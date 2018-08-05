aa3c6018fb700f2b11b4717ec203b36566b34c02
Bryan Budreau <bryan.budreau@gmail.com>
2018-08-04 20:14:09 -0500

    first commit

---
+++ IdeaProjects/cappy/src/main/java/com/example/cappy/CappyApplication.java
@@ -0,0 +1,12 @@
+package com.example.cappy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CappyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CappyApplication.class, args);
+	}
+}
