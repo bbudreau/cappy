c5cc35d230ea2431f9710422005ac25bc76257e7
Bryan Budreau <bryan.budreau@gmail.com>
2018-08-07 20:09:02 -0500

    added nutrition info and styles.css

--- IdeaProjects/cappy/src/main/java/com/example/cappy/Controllers/NutritionController.java
+++ IdeaProjects/cappy/src/main/java/com/example/cappy/Controllers/NutritionController.java
@@ -6,32 +6,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 import java.util.ArrayList;
-
 @Controller
-@RequestMapping(value = "/home/workout")
-public class WorkoutController {
-
-    static ArrayList<String> lifts = new ArrayList<>();
-    @RequestMapping("")
+@RequestMapping(value = "/home/nutrition")
+public class NutritionController {
+    static ArrayList<String> foods = new ArrayList<>();
+    @RequestMapping(value = "")
     public String index (Model model){
-
-        model.addAttribute("lifts", lifts);
-        model.addAttribute("Title", "Build a workout");
-        return "workout/workout";
-}
+        model.addAttribute("foods", foods);
+        model.addAttribute("title", "Track your Calories");
+        return "/nutrition/nutrition";
+    }
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddWorkoutForm(Model model){
-        model.addAttribute("title", "Add Workout");
-        return "workout/add";
+        model.addAttribute("title", "Add Food");
+        return "nutrition/add";
     }
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddWorkoutForm(@RequestParam String liftName){
-        lifts.add(liftName);
-        return"redirect:";
-
-
+    public String processAddWorkoutForm(@RequestParam String foodName) {
+        foods.add(foodName);
+        return "redirect:";
     }
 }
-
